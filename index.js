e5e139f19693f05141d8c453d4509b90eed7dba5
nemostatus <mattcorralejo1@gmail.com>
2021-01-22 22:57:38 -0800

    added a condition to function

--- index.js
+++ index.js
@@ -7,8 +7,14 @@ let display = document.getElementById("display")
 if(date.getHours()<12 && date.getHours()>1 ){
 display.innerHTML = "GOOD MORNING"
 }
-else{
-    display.innerHTML = "HELLO"
+else if(date.getHours()<16 && date.getHours()>12){
+    display.innerHTML = "GOOD AFTERNOON"
+}
+else if(date.getHours()<19 && date.getHours()>16){
+    display.innerHTML = "GOOD AFTERNOON"
+}
+else if(date.getHours()<24 && date.getHours()>19){
+    display.innerHTML = "HELLO NIGHT OWL"
 }
 }
 
