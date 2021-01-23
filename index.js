18cc81b7562715181ededdf79cd77793299a7b7a
nemostatus <mattcorralejo1@gmail.com>
2021-01-23 00:16:33 -0800

    starting to work

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-document.addEventListener("keypress", cat)
+document.addEventListener("keypress", cat);
+
 function checkTime(){
     console.log("rfre")
 let date = new Date()
+console.log(date)
 let display = document.getElementById("display")
+console.log(display)
 if(date.getHours()<12 && date.getHours()>1 ){
 display.innerHTML = "GOOD MORNING"
 }
@@ -12,7 +15,7 @@ else if(date.getHours()<16 && date.getHours()>12){
 else if(date.getHours()<19 && date.getHours()>16){
     display.innerHTML = "GOOD EVENING"
 }
-else if(date.getHours()<24 && date.getHours()>19){
+else {
     display.innerHTML = "HELLO NIGHT OWL"
 }
 }
